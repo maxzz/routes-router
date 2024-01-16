e3994cdb6c210016b4ecaeb296c8e2e24b68c0f1
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-15 16:31:05 -0800

    WIP. add simples router w/ error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([{
+    path: "/",
+    element: <div className="h-screen bg-sky-300">123</div>,
+    errorElement: <div>404</div>,
+}]);
+
 export function App() {
     return (
-        <div className="h-screen bg-sky-300">
-            123
-        </div>
+        <RouterProvider router={router} />
     );
 }
