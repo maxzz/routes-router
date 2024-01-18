a2b82683c192ab7e7d8f6dcbd8ac8173f7b590ea
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-18 14:37:09 -0800

    WIP. Header. update links

--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -2,17 +2,22 @@ import { HTMLAttributes } from 'react';
 import { classNames } from '@/utils/classnames';
 import { Link, useLocation } from 'react-router-dom';
 
+const linkClasses = (active: boolean) => classNames(
+    "px-1 py-2  hover:text-sky-200 hover:bg-sky-400 transition-colors duration-200 ease-in-out",
+    active ? "text-sky-50" : "text-sky-600"
+);
+
 export function Header({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     const loc = useLocation();
     return (
-        <div className={classNames("px-4 py-2 text-sky-500 bg-sky-400/50 border-sky-400 border-b shadow flex items-center justify-between", className)} {...rest}>
-            <div className="">Logo</div>
+        <div className={classNames("px-4 font-semibold text-sky-500 bg-sky-400/50 border-sky-400 border-b shadow flex items-center justify-between", className)} {...rest}>
+            <div className="text-sky-900">Routes</div>
 
             <div className="flex gap-2">
-                <Link to="/about" className={classNames(loc.pathname === "/about" && "text-sky-200")}>About</Link>
-                <Link to="/books" className={classNames(loc.pathname === "/books" && "text-sky-200")}>Books</Link>
-                <Link to="/contact" className={classNames(loc.pathname === "/contact" && "text-sky-200")}>Contact</Link>
+                <Link to="/about" className={classNames(linkClasses(loc.pathname === "/about"))}>About</Link>
+                <Link to="/books" className={classNames(linkClasses(loc.pathname === "/books"))}>Books</Link>
+                <Link to="/contact" className={classNames(linkClasses(loc.pathname === "/contact"))}>Contact</Link>
             </div>
-        </div>
+        </div >
     );
 }
