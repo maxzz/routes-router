8ad409584dee7927f31c28fa836e85758f9db40b
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-19 15:24:23 -0800

    WIP. carousel. ---------- replace all css styles w/ tw

--- src/ui/carousel/EmblaCarouselArrowButtons.tsx
+++ src/ui/carousel/EmblaCarouselArrowButtons.tsx
@@ -1,5 +1,6 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { EmblaCarouselType as CarouselApi } from 'embla-carousel';
+import { IconNext, IconPrev } from '../icons';
 
 type UsePrevNextButtonsType = {
     prevBtnDisabled: boolean;
@@ -56,12 +57,8 @@ type ButtonProps = PropsWithChildren<DetailedHTMLProps<ButtonHTMLAttributes<HTML
 
 export function PrevButton({ children, ...rest }: ButtonProps) {
     return (
-        <button className="embla__button embla__button--prev" type="button" {...rest}>
-            <svg className="embla__button__svg" viewBox="0 0 532 532">
-                <path
-                    fill="currentColor"
-                    d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z" />
-            </svg>
+        <button className="" type="button" {...rest}> {/* embla__button embla__button--prev */}
+            <IconPrev className="w-8 h-8 fill-current" /> {/* "embla__button__svg" */}
             {children}
         </button>
     );
@@ -69,12 +66,8 @@ export function PrevButton({ children, ...rest }: ButtonProps) {
 
 export function NextButton({ children, ...restProps }: ButtonProps) {
     return (
-        <button className="embla__button embla__button--next" type="button" {...restProps}>
-            <svg className="embla__button__svg" viewBox="0 0 532 532">
-                <path
-                    fill="currentColor"
-                    d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z" />
-            </svg>
+        <button className="" type="button" {...restProps}> {/* embla__button embla__button--next */}
+            <IconNext className="w-8 h-8 fill-current" /> {/* "embla__button__svg" */}
             {children}
         </button>
     );
