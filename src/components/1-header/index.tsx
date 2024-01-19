3c9e3bb5a3780cebe4824e5b8b39144e85d452ea
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-18 16:31:06 -0800

    WIP. Header. tweaks routes selection

--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -4,20 +4,20 @@ import { Link, useLocation } from 'react-router-dom';
 import { IconRoutes } from '@/ui/icons';
 
 const linkClasses = (active: boolean) => classNames(
-    "px-1 py-2  hover:text-sky-200 hover:bg-sky-400 transition-colors duration-200 ease-in-out",
+    "px-3 py-2 pb-3 tracking-wide hover:text-sky-100 hover:bg-sky-700 transition-colors duration-200 ease-in-out",
     active ? "text-sky-50" : "text-sky-600"
 );
 
 export function Header({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     const loc = useLocation();
     return (
-        <div className={classNames("px-4 font-semibold text-sky-500 bg-sky-400/50 border-sky-400 border-b shadow flex items-center justify-between", className)} {...rest}>
-            <div className="text-sky-400 flex items-center">
-                <IconRoutes className="w-6 h-6 fill-current stroke-sky-500 strike-[2]" />
-                <div className="[-webkit-text-stroke:1px_#0ea5e9]">Routes</div>
+        <div className={classNames("px-4 font-semibold text-sky-500 bg-sky-900 border-sky-600 border-b shadow flex items-center justify-between", className)} {...rest}>
+            <div className="tracking-wide text-sky-400 flex items-center">
+                <IconRoutes className="w-6 h-6 fill-current stroke-green-700 stroke-[.4]" />
+                <div className="[-webkit-text-stroke:0.4px_#15803d]">Routes</div>
             </div>
 
-            <div className="flex gap-2">
+            <div className="flex items-center">
                 <Link to="/about" className={classNames(linkClasses(loc.pathname === "/about"))}>About</Link>
                 <Link to="/books" className={classNames(linkClasses(loc.pathname === "/books"))}>Books</Link>
                 <Link to="/contact" className={classNames(linkClasses(loc.pathname === "/contact"))}>Contact</Link>
