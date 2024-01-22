c7916be242b18cc5c9503bf3923f045b11ac6b3a
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-21 18:36:23 -0800

    WIP. update footer

--- src/components/0-router/2-home.tsx
+++ src/components/0-router/2-home.tsx
@@ -12,25 +12,13 @@ border rounded shadow \
 active:scale-x-[.97] \
 transition-colors";
 
-const anchorClasses = "\
-border-transparent border-dotted \
-border-b \
-hover:border-current \
-";
-
-const isDev = process.env.NODE_ENV === "development";
-
 export function HomePage() {
     return (
-        <div className="h-full bg-sky-800 flex flex-col">
+        <div className="h-full bg-sky-800">
             <div className="flex-1">
                 <Link to="/" className={linkClasses}>
                     Choose a route
                 </Link>
-            </div>
-            <div className="px-4 py-3 text-sm text-sky-400 flex gap-4">
-                <a className={anchorClasses} href={isDev ? "/" : "https://maxzz.github.io/routes-router/"}>./routes-router</a>
-                <a className={anchorClasses} href="https://github.com/maxzz/routes-router" target="blank" referrerPolicy="no-referrer">source code</a>
             </div>
         </div>
     );
--- src/components/3-footer/index.tsx
+++ src/components/3-footer/index.tsx
@@ -10,16 +10,30 @@ import { IconSunnyvale } from '@/ui/icons';
 //     );
 // }
 
+const anchorClasses = "\
+border-transparent border-dotted \
+border-b \
+hover:border-current \
+";
+
+const isDev = process.env.NODE_ENV === "development";
+
 export function Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
         <div className={classNames("pt-1 pb-1.5 text-xs text-slate-600 bg-slate-950 flex items-center justify-center", className)} {...rest}>
             <p>Created by Max Zakharzhevskiy.</p>
             <IconSunnyvale className="w-5 h-5" />
             <p>Open sourced on{' '}
-                <a className="border-current border-b border-dotted" href="https://github.com/maxzz/forms-formik" target="_blank" rel="noopener">
+                <a className="border-current border-b border-dotted" href="https://github.com/maxzz/routes-router" target="_blank" rel="noopener" referrerPolicy="no-referrer">
                     GitHub.
                 </a>
             </p>
+
+            <div className="px-4 py-3 text-xs text-sky-300/50 flex gap-4">
+                <a className={anchorClasses} href={isDev ? "/" : "https://maxzz.github.io/routes-router/"}>./routes-router</a>
+                <a className={anchorClasses} href="https://github.com/maxzz/routes-router" target="_blank" referrerPolicy="no-referrer" rel="noopener">source code</a>
+            </div>
+
         </div>
     );
 }
