54e13706b8dbd00dc255e8f22946ee2527c9c216
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-21 18:43:18 -0800

    WIP. update footer layout

--- src/components/3-footer/index.tsx
+++ src/components/3-footer/index.tsx
@@ -20,18 +20,25 @@ const isDev = process.env.NODE_ENV === "development";
 
 export function Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={classNames("pt-1 pb-1.5 text-xs text-slate-600 bg-slate-950 flex items-center justify-center", className)} {...rest}>
+        <div className={classNames("py-0.5 text-xs text-sky-300/50 bg-sky-900 border-sky-700 border-t flex items-center justify-center", className)} {...rest}>
             <p>Created by Max Zakharzhevskiy.</p>
+
             <IconSunnyvale className="w-5 h-5" />
-            <p>Open sourced on{' '}
-                <a className="border-current border-b border-dotted" href="https://github.com/maxzz/routes-router" target="_blank" rel="noopener" referrerPolicy="no-referrer">
+
+            <p>Source code on{' '}
+                <a
+                    className="border-current border-b border-dotted"
+                    href="https://github.com/maxzz/routes-router"
+                    target="_blank"
+                    rel="noopener"
+                    referrerPolicy="no-referrer"
+                >
                     GitHub.
                 </a>
             </p>
 
-            <div className="px-4 py-3 text-xs text-sky-300/50 flex gap-4">
+            <div className="px-4 py-3 text-xs text-sky-300/70">
                 <a className={anchorClasses} href={isDev ? "/" : "https://maxzz.github.io/routes-router/"}>./routes-router</a>
-                <a className={anchorClasses} href="https://github.com/maxzz/routes-router" target="_blank" referrerPolicy="no-referrer" rel="noopener">source code</a>
             </div>
 
         </div>
